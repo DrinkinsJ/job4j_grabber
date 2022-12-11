3caf4c4189709ae81bcc67e1ab09b7e4820da4f7
drinkinsj <Drinkinswo@gmail.com>
2022-12-11 18:26:55 +0300

    https://job4j.ru/profile/exercise/56/task/356/369469 fix and add test

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void dateTimeParserReturnLocalDayTime(){
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        String expected = "2022-12-11T16:27:56";
+        assertThat(dateTimeParser.parse("2022-12-11T16:27:56+03:00").toString()).isEqualTo(expected);
+    }
+}
